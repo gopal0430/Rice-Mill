c2c5f813ed4b29b31e12d18ce8dc2653b5df3244
“GokulKarupusamy” <gokulkps007@gmail.com.com>
2024-09-17 10:30:01 +0530

    pages added

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ document.addEventListener("click",function (e){
     myModal.show();
    }
 })
+document.addEventListener('DOMContentLoaded', function() {
+   // Show the loading icon
+   const loadingIcon = document.getElementById('loading');
+   loadingIcon.style.display = 'none'; // Initially hidden
+   
+   // Show the loading icon before page unload
+   window.addEventListener('beforeunload', function() {
+     loadingIcon.style.display = 'flex';
+   });
+ });
